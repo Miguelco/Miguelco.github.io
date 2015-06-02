b3fc8ec7d72aa86713216742743b50acb94810e2
Miguel Covarrubias <migcovarrubias@ucdavis.edu>
2015-06-02 14:48:32 -0700

    7

--- dat.php
+++ dat.php
@@ -1,5 +1,9 @@
 
-
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
 	<?php
 	$GameDataInG = $_GET['g'];
 	$GameDataInP = $_POST['p'];
@@ -7,4 +11,8 @@
 	$curLog = file_get_contents('log.txt');
 	$curLog .= "G: ".$GameDataInG." -- P: ". $GameDataInP ."\n\n";
 	file_put_contents('log.txt', $curLog);
+	
+	 echo  "meco";
 	?>
+ </body>
+</html>
